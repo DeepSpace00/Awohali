98175ba8b0250bab087bb4c71dd62b4a15a37857
DeepSpace00 <deepspace00@pm.me>
2025-08-13 10:49:09 -0400

    synced with basic_usage example

--- firmware/drivers/bq25798/src/bq25798_platform.h
+++ firmware/drivers/bq25798/src/bq25798_platform.h
@@ -1,15 +1,26 @@
+/**
+ * @file bq25798_platform.h
+ * @brief Platform abstraction layer for BQ25798 I2C driver
+ * @author Madison Gleydura (DeepSpace00)
+ * @date 2025-07-18
+ */
+
 #ifndef BQ25798_PLATFORM_H
 #define BQ25798_PLATFORM_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
-#include <stdbool.h>
 
-typedef enum {
-    BQ25798_OK = 0,
-    BQ25798_ERROR = -1
-} BQ25798_Status;
+// Platform-specific I2C functions
+int platform_i2c_write(uint8_t dev_addr, const uint8_t *data, uint16_t len);
+int platform_i2c_read(uint8_t dev_addr, uint8_t *data, uint16_t len);
+void platform_delay_ms(uint32_t ms);
 
-BQ25798_Status bq25798_i2c_read(uint8_t dev_addr, uint8_t reg, uint8_t *data, uint16_t len);
-BQ25798_Status bq25798_i2c_write(uint8_t dev_addr, uint8_t reg, const uint8_t *data, uint16_t len);
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BQ25798_PLATFORM_H
