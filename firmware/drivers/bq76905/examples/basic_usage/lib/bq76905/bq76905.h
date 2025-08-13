85f4475c5cfe0447e9d38d3ae08641aa44b41904
DeepSpace00 <deepspace00@pm.me>
2025-08-13 12:56:30 -0400

    added settings struct pointer

--- firmware/drivers/bq76905/examples/basic_usage/lib/bq76905/bq76905.h
+++ firmware/drivers/bq76905/examples/basic_usage/lib/bq76905/bq76905.h
@@ -468,7 +468,7 @@ bq76905_status_t bq76905_sleep_disable(bq76905_t *dev);
 
 ///< @section Subcommands with Data
 
-bq76905_status_t bq76905_get_device_number(bq76905_t *dev, uint16_t *device_number);
+bq76905_status_t bq76905_get_device_number(bq76905_t *dev, bq76905_settings_t *settings);
 
 bq76905_status_t bq76905_get_firmware_version(bq76905_t *dev, uint16_t *firmware_version, uint16_t *build_number);
 
