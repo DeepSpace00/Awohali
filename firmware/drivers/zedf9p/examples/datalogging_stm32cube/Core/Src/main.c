85d096e20cd22979c1b2136f01fc8a7af83ba650
DeepSpace00 <deepspace00@pm.me>
2025-09-23 11:58:47 -0400

    measurement rate

--- firmware/drivers/zedf9p/examples/datalogging_stm32cube/Core/Src/main.c
+++ firmware/drivers/zedf9p/examples/datalogging_stm32cube/Core/Src/main.c
@@ -356,7 +356,7 @@ void configure_gnss_for_logging(void) {
     usb_debug_print("Configuring GNSS for RAWX and HPPOSLLH logging...\r\n");
 
     // Set measurement rate to 1Hz
-    zedf9p_set_measurement_rate(&gnss_module, UBLOX_CFG_LAYER_RAM, 1000, 1);
+    zedf9p_set_measurement_rate(&gnss_module, UBLOX_CFG_LAYER_RAM, 5000, 1);
 
     // Configure GPS + Galileo for optimal RAWX data
     const zedf9p_gnss_config_t gnss_config = {
