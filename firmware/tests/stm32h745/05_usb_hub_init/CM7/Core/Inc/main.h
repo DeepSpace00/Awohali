76b08891ec53604f37f991a2fe1a2a74fee6fe86
DeepSpace00 <deepspace00@pm.me>
2025-10-15 14:18:28 -0400

    added USB stuff to main.h

--- firmware/tests/stm32h745/05_usb_hub_init/CM7/Core/Inc/main.h
+++ firmware/tests/stm32h745/05_usb_hub_init/CM7/Core/Inc/main.h
@@ -31,12 +31,34 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "usb2422.h"
 
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
+typedef struct {
+  uint32_t session_start_ms;
+  uint32_t last_flush_ms;
+  uint32_t last_stats_ms;
+  uint32_t bytes_logged;
+  uint32_t messages_logged;
+  uint32_t clock_count;
+  uint32_t hpposecef_count;
+  uint32_t hpposllh_count;
+  uint32_t timeutc_count;
+  uint32_t rawx_count;
+  uint32_t sfrbx_count;
+  uint32_t file_number;
+  uint8_t logging_active;
+  uint8_t sd_card_present;
+  uint32_t write_errors;
+  uint32_t recovery_attempts;
+  uint8_t usb_ready;
+  uint8_t file_open;
+} logging_stats_t;
 
+extern logging_stats_t logging_stats;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
@@ -53,20 +75,24 @@ extern "C" {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
+void usb_debug_print(const char *message);
 
+usb2422_status_t configure_usb2422_for_enumeration(usb2422_t *dev);
+void print_hub_configuration(usb2422_t *dev);
+
+extern usb2422_hub_settings_t hub_settings;
+extern usb2422_power_settings_t power_settings;
+extern usb2422_downstream_port_settings_t port_settings;
+extern usb2422_cfg_regs_t config_registers;
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define GNSS_ENABLE_Pin GPIO_PIN_10
-#define GNSS_ENABLE_GPIO_Port GPIOE
-#define GNSS_TXRDY_Pin GPIO_PIN_11
-#define GNSS_TXRDY_GPIO_Port GPIOE
-#define GNSS_INT_Pin GPIO_PIN_12
-#define GNSS_INT_GPIO_Port GPIOE
-#define GNSS_PPS_Pin GPIO_PIN_13
-#define GNSS_PPS_GPIO_Port GPIOE
-#define GNSS_RESET_Pin GPIO_PIN_14
-#define GNSS_RESET_GPIO_Port GPIOE
+#define HUB_SUSPEND_Pin GPIO_PIN_0
+#define HUB_SUSPEND_GPIO_Port GPIOB
+#define RESET_HUB_Pin GPIO_PIN_1
+#define RESET_HUB_GPIO_Port GPIOB
+#define HUB_H7_INT_Pin GPIO_PIN_2
+#define HUB_H7_INT_GPIO_Port GPIOB
 #define L4_RESET_Pin GPIO_PIN_3
 #define L4_RESET_GPIO_Port GPIOD
 #define L4_BOOT_Pin GPIO_PIN_4
