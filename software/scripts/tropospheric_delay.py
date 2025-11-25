16a66776bda960a4eacd9d1c128e06cff137e23e
DeepSpace00 <deepspace00@pm.me>
2025-11-25 16:11:00 -0500

    calc for new data

--- software/scripts/tropospheric_delay.py
+++ software/scripts/tropospheric_delay.py
@@ -16,28 +16,28 @@ f_GPS_L5_Hz = 1176.450e6
 
 c = 299792458.0 # Speed of light (m/s)
 
-json_file = "ephemerides/ephemeris_sample_2025-10-03.json"
-utc_time = datetime(2025, 10, 3, 22, 43, 45, tzinfo=timezone.utc)
-receiver_ecef = (863798.932, -5503182.842, 3095969.847) # Apartment
+json_file = "ephemerides/ephemeris_sample_2025-11-25.json"
+utc_time = datetime(2025, 11, 25, 15, 29, 32, tzinfo=timezone.utc)
+receiver_ecef = (867068.487, -5504812.066, 3092176.505) # Apartment
 
 # RXM-RAWX receiver time of week and week number
-rcvTOW = 513845.005
-gps_week = 2386
-sat_id = 'G04'
+rcvTOW = 228590.993
+gps_week = 2394
+sat_id = 'E36'
 
 # Convert receiver time to GPS system time
 gps_tow = rcvTOW
 
 # Receiver clock biases (from NAV-CLOCK)
-receiver_clock_bias = 579918 # ns
-receiver_clock_drift = 545 # ns/s
-iTOW = 513844000
+receiver_clock_bias = 352931 # ns
+receiver_clock_drift = 485 # ns/s
+iTOW = 228590000
 
-measured_pseudorange_L1 = 21585931.4032699 # meters
-measured_pseudorange_L5 = 21585913.2487915 # meters
+measured_pseudorange_L1 = 22121216.9695235 # meters
+measured_pseudorange_L5 = 22121211.5424739 # meters
 
-measured_pseudorange_E1 = 25332868.9768342 # meters
-measured_pseudorange_E5a = 25332853.5543619 # meters
+measured_pseudorange_E1 = 24638154.25013 # meters
+measured_pseudorange_E5a = 24638147.5472601 # meters
 
 # Calculate receiver clock bias at measurement time (RXM-RAWX and NAV-CLOCK messages may be slightly out of sync)
 dt = rcvTOW - (iTOW / 1000.0)
@@ -67,7 +67,7 @@ biases = rcv_clkBias_m - sat_clkBias_m - relativistic_bias_m
 # Calculate combined pseudorange for multi-band measurements
 combined_pseudorange_GAL = (((f_Galileo_E1_Hz ** 2) * measured_pseudorange_E1) - (f_Galileo_E5a_Hz ** 2) * measured_pseudorange_E5a) / (f_Galileo_E1_Hz ** 2 - f_Galileo_E5a_Hz ** 2)
 combined_pseudorange_GPS = (((f_GPS_L1_Hz ** 2) * measured_pseudorange_L1) - (f_GPS_L5_Hz ** 2) * measured_pseudorange_L5) / (f_GPS_L1_Hz ** 2 - f_GPS_L5_Hz ** 2)
-combined_pseudorange = combined_pseudorange_GPS
+combined_pseudorange = combined_pseudorange_GAL
 
 # Calculate tropospheric delay (residuals)
 tropospheric_delay_m = combined_pseudorange - geometric_range - biases
