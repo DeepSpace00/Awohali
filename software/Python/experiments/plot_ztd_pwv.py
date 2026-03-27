0b8929afca4e5c209a1c094965d873b86a9c924d
DeepSpace00 <deepspace00@pm.me>
2026-03-27 07:42:59 -0400

    LaTeX plotting

--- software/Python/experiments/plot_ztd_pwv.py
+++ software/Python/experiments/plot_ztd_pwv.py
@@ -1,8 +1,31 @@
 import math
 import sqlite3
 import pandas as pd
-import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib
+matplotlib.use('pgf')
+import matplotlib.pyplot as plt
+import numpy as np
+
+# PGF/LaTeX rendering configuration.
+matplotlib.rcParams.update({
+    'pgf.texsystem': 'pdflatex',
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+    'font.size': 30,
+    'axes.titlesize': 30,
+    'axes.labelsize': 30,
+    'xtick.labelsize': 30,
+    'ytick.labelsize': 30,
+    'legend.fontsize': 30,
+    'figure.titlesize': 30,
+    'pgf.preamble': r"""
+        \usepackage[utf8]{inputenc}
+        \usepackage[T1]{fontenc}
+    """,
+})
+
 
 _DATA = Path(__file__).parent.parent / "data"
 
@@ -31,9 +54,9 @@ fig, ax = plt.subplots(
     layout="constrained"
 )
 
-ax[0].plot(rinex_data['rcvTOW'], rinex_data['ZTD_m'], label='RINEX ZTD', color='green')
+ax[0].plot(rinex_data['rcvTOW'], rinex_data['ZTD_m'], label='RINEX ZTD', color='green', linewidth=3)
 ax0r = ax[0].twinx()
-ax0r.plot(ubx_data['rcvTOW'], ubx_data['ZTD_m'], label='UBX ZTD', color='blue')
+ax0r.plot(ubx_data['rcvTOW'], ubx_data['ZTD_m'], label='UBX ZTD', color='blue', linewidth=3)
 ax[0].set_ylabel("RINEX ZTD (m)")
 ax0r.set_ylabel("UBX ZTD (m)")
 ax[0].set_title("Zenith Time Delay (ZTD)")
@@ -44,12 +67,11 @@ ax[0].legend(lines, labels, loc="upper right")
 ax[0].set_xlim(x_min, x_max)
 ax[0].set_ylim(rinex_data['ZTD_m'].min(), rinex_data['ZTD_m'].max())
 ax0r.set_ylim(ubx_data['ZTD_m'].min(), ubx_data['ZTD_m'].max())
+ax[0].set_yticks(np.arange(1.5, 3.6, 0.5))
 
-plt.grid()
-
-ax[1].plot(rinex_data['rcvTOW'], rinex_data['PWV_mm'], label='RINEX PWV', color='green')
+ax[1].plot(rinex_data['rcvTOW'], rinex_data['PWV_mm'], label='RINEX PWV', color='green', linewidth=3)
 ax1r = ax[1].twinx()
-ax1r.plot(ubx_data['rcvTOW'], ubx_data['PWV_mm'], label='UBX PWV', color='blue')
+ax1r.plot(ubx_data['rcvTOW'], ubx_data['PWV_mm'], label='UBX PWV', color='blue', linewidth=3)
 ax[1].set_ylabel("RINEX PWV (mm)")
 ax1r.set_ylabel("UBX PWV (mm)")
 ax[1].set_title("Precipitable Water Vapor (PWV)")
@@ -59,13 +81,13 @@ ax[1].legend(lines, labels, loc="upper right")
 
 ax[1].set_ylim(rinex_data['PWV_mm'].min(), rinex_data['PWV_mm'].max())
 ax1r.set_ylim(ubx_data['PWV_mm'].min(), ubx_data['PWV_mm'].max())
+ax[1].set_yticks(np.arange(-1, 8, 1))
 
-plt.grid()
+
+ax[1].set_xticks(np.arange(226800.0, 236000.0, 1000))
 
 ax[1].set_xlabel("Receiver TOW (s)")
 
-fig.suptitle("Tropospheric Products - epoch_summary")
+fig.suptitle("GNSS Tropospheric Products")
 
-plt.savefig(f"{_DATA}/figures/troposphericProducts_test_3/epoch_summary.png")
-
-plt.show()
+plt.savefig(f"{_DATA}/figures/troposphericProducts_test_3/epoch_summary_3.png")
