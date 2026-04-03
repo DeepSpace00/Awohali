e8ff21f5e9f83f2b31f4b778ee2184dce8e0b342
DeepSpace00 <deepspace00@pm.me>
2026-04-03 07:18:45 -0400

    removed old scripts

--- software/Python/experiments/rasterio_test.py
+++
@@ -1,9 +0,0 @@
-import rasterio
-
-with rasterio.open('/home/deepspace/Downloads/Monongalia_County_2025.sid') as src:
-    data = src.read()
-    profile = src.profile
-    profile.update(driver='GTiff')
-
-    with rasterio.open('/home/deepspace/Downloads/Monongalia_County_2025.tif', 'w', **profile) as dst:
-        dst.write(data)
